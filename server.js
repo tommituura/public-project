4a6fa905dcef238f1a089d7a7f99361dc73f7eef
Tommi Tuura <tommi.tuura@helsinki.fi>
2020-06-02 16:08:49 +0300

    added some simple logging, a newline for curl-friendliness, setting port with env variable

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-var http = require('http');
+const http = require('http');
+
+const SERVER_PORT = process.env['SERVER_PORT'] || 8080;
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
+  console.log(`Saying hello to ${req.connection.remoteAddress}`);
+  res.write("Hello World!\n"); //write a response to the client
   res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080 
+}).listen(SERVER_PORT);
+
+console.log(`Listening on ${SERVER_PORT}.`);
